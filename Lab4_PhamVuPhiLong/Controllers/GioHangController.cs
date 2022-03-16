309272136a387938b5382b1f6a1a80b04f9ae44d
LDK <23lop95phamvuphilong@gmail.com>
2022-03-16 15:32:37 +0700

    Add project files.

---
+++ Lab4_PhamVuPhiLong/Controllers/GioHangController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab4_PhamVuPhiLong.Models;
+
+namespace Lab4_PhamVuPhiLong.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        MyDataDataContext data = new MyDataDataContext();
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if(lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.Find(n => n.masach == id);
+            if(sanpham == null)
+            {
+                sanpham = new GioHang(id);
+                lstGioHang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoLuong++;
+                return Redirect(strURL);
+            }
+        }
+
+        private int TongSoLuong()
+        {
+            int tsl = 0;
+            List<GioHang> lstGiohang = Session["GioHang"] as List<GioHang>;
+            if(lstGiohang != null)
+            {
+                tsl = lstGiohang.Sum(n => n.iSoLuong);
+            }
+            return tsl;
+        }
+        private int TongSoLuongSanPham()
+        {
+            int tsl = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if(lstGioHang != null)
+            {
+                tsl = lstGioHang.Count;
+            }
+            return tsl;
+        }
+
+        private double TongTien()
+        {
+            double tt = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if(lstGioHang != null)
+            {
+                tt = lstGioHang.Sum(n => n.dThanhtien);
+            }
+            return tt;
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            ViewBag.TongSoLuongSanPham = TongSoLuongSanPham();
+            return View(lstGioHang);
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            ViewBag.TongSoLuongSanPham = TongSoLuongSanPham();
+            return PartialView();
+        }
+
+        public ActionResult XoaGioHang(int id)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.masach == id);
+            if (sanpham != null)
+            {
+                lstGioHang.RemoveAll(n => n.masach == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapnhatGioHang(int id, FormCollection collection)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.masach == id);
+            if(sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(collection["txtSoLg"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoaTatCagioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            lstGioHang.Clear();
+            return RedirectToAction("GioHang");
+        }
+
+    }
+}
---
+++ Lab4_PhamVuPhiLong/Models/GioHang.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Lab4_PhamVuPhiLong.Models;
+
+namespace Lab4_PhamVuPhiLong.Models
+{
+    public class GioHang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int masach { get; set; }
+
+        [Display(Name = "Tên Sách")]
+        public string tensach { get; set; }
+
+        [Display(Name = "Ảnh Bìa")]
+        public string hinh { get; set; }
+
+        [Display(Name = "Giá Bán")]
+        public Double giaban { get; set; }
+
+        [Display(Name = "Số Lượng")]
+        public int iSoLuong { get; set; }
+
+        [Display(Name = "Thành tiền")]
+        public Double dThanhtien
+        {
+            get { return iSoLuong * giaban; }
+        }
+
+        public GioHang (int id)
+        {
+            masach = id;
+            Sach sach = data.Saches.Single(n => n.masach == masach);
+            tensach = sach.tensach;
+            hinh = sach.hinh;
+            giaban = double.Parse(sach.giaban.ToString());
+            iSoLuong = 1;
+        }
+
+    }
+}
